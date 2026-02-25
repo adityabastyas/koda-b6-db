669cad5c31520e74a9c2bbf74091c1ab551c507b
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-02-25 15:23:40 +0700

    feat: add implementation subqueri, select in select

--- query.sql
+++ query.sql
@@ -18,5 +18,33 @@ JOIN "product_variant" ON "products"."product_id" = "product_variant"."product_i
 JOIN "product_size" ON "products"."product_id" = "product_size"."product_id"
 WHERE "products"."product_id" = 1
 AND "product_variant"."variant_id" = 2
-AND "product_size"."product_size_id" = 3
+AND "product_size"."product_size_id" = 3;
 
+---
+
+--Query untuk menambahkan subtotal dari barang yang dipilih, termasuk yang tadi
+-- caranya pakai yang tadi
+--expetasi sub total , quantity
+--- quantity + subtotal
+---  2            ---
+--   5            ---
+---  7            ---
+
+SELECT
+    "cart_item"."quantity",
+    "cart_item"."quantity" *
+    (
+        SELECT
+            "products"."price"
+            + "product_variant"."add_price"
+            + "product_size"."add_price"
+        FROM "products"
+        JOIN "product_variant"
+            ON "products"."product_id" = "product_variant"."product_id"
+        JOIN "product_size"
+            ON "products"."product_id" = "product_size"."product_id"
+        WHERE "products"."product_id" = "cart_item"."product_id"
+        AND "product_variant"."variant_id" = "cart_item"."variant_id"
+        AND "product_size"."product_size_id" = "cart_item"."product_size_id"
+    ) AS "sub_total"
+FROM "cart_item";
