73a1eb32b7be557863df939194e645759cc737fb
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-02-25 12:04:59 +0700

    feat: add new file query.sql and display product, final_price, variant, size

---
+++ query.sql
@@ -0,0 +1,11 @@
+SELECT 
+   "products"."name" AS "nama_product", 
+   "products"."price" + "product_variant"."add_price" + "product_size"."add_price" AS "final_price",
+   "product_variant"."temperature" AS "variant",
+   "product_size"."name" AS "size"
+FROM "products"
+JOIN "product_variant" ON "products"."product_id" = "product_variant"."product_id"
+JOIN "product_size" ON "products"."product_id" = "product_size"."product_id"
+WHERE "products"."product_id" = 1
+AND "product_variant"."temperature" = 'Hot'
+AND "product_size"."name" = 'Large';
